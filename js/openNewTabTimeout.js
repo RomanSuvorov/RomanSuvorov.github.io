544436a82f89f42dd7b6638655dd82fd85385ff5
Roman Suvorov <roma232789@gmail.com>
2023-02-07 18:42:54 +0200

    test 8

--- js/openNewTabTimeout.js
+++ js/openNewTabTimeout.js
@@ -5,16 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     bodySelector.style.backgroundColor = 'rgba(18,52,5,0.8)';
     bodySelector.style.color = '#ded9d9';
     console.info('DOM loaded successful');
+    // bodySelector.append()
     
     const isBlocked = checkIsPopupBlocked();
     console.log('isBlocked', isBlocked);
-    if (isBlocked) {
-        const errorSelector = document.getElementsByClassName('error')[0];
-    
-        errorSelector.textContent = 'Please, disable your popup blocker!';
-    } else {
+    // if (isBlocked) {
+    //     const errorSelector = document.getElementsByClassName('error')[0];
+    //
+    //     errorSelector.textContent = 'Please, disable your popup blocker!';
+    // } else {
         timer();
-    }
+    // }
     
     const link = window.document.createElement('a');
     link.target = '_blank';
@@ -37,7 +38,7 @@ const checkIsPopupBlocked = () => {
     }
 }
 
-const timer = (initSeconds = 2)  => {
+const timer = (initSeconds = 4)  => {
     let secondsForTimeout = initSeconds * 1000;
     const intervalSeconds = 1000;
 
@@ -55,29 +56,6 @@ const openNewTab = (url = URL) => {
     const link = window.document.createElement('a');
     link.target = '_blank';
     link.href = url;
-    // const e = window.document.createEvent("MouseEvents");
-    // e.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    
-    const event = new MouseEvent(
-        'click',
-        {
-            screenX: 0,
-            screenY: 0,
-            clientX: 0,
-            clientY: 0,
-            ctrlKey: false,
-            shiftKey: false,
-            altKey: false,
-            metaKey: false,
-            button: 0,
-            relatedTarget: null,
-            detail: 0,
-            view: window,
-            cancelable: true,
-            bubbles: true,
-        });
-    const result = link.dispatchEvent(event);
-    console.log('result', result);
 }
 
 const openNewTabViaButton = () => {
@@ -102,11 +80,15 @@ const openNewTabViaButton = () => {
             cancelable: true,
             bubbles: true,
         });
-    link.dispatchEvent(event);
+    // link.dispatchEvent(event);
     // const a = link.click();
     // console.log('result', a);
     // window.open(a, '_blank');
     // window.location.replace(URL);
+    const pathname = window.location.pathname.split('/');
+    console.log(window.location.pathname.split('/'));
+    window.location.assign('../templates/page1.html');
+    // history.pushState(null, '', '')
 }
 
 const showTimer = (seconds) => {
